18d05700e5597c9671ab842ec32e577c8a184c45
Maximus Rosa <maxborgesrosa@gmail.com>
2024-12-24 22:57:46 -0300

    texture mapping beginning

---
+++ src/graphics/textures.cpp
@@ -0,0 +1,95 @@
+// Arquivos "headers" padrões de C podem ser incluídos em um
+// programa C++, sendo necessário somente adicionar o caractere
+// "c" antes de seu nome, e remover o sufixo ".h". Exemplo:
+//    #include <stdio.h> // Em C
+//  vira
+//    #include <cstdio> // Em C++
+//
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+// Headers abaixo são específicos de C++
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+
+// Headers das bibliotecas OpenGL
+#include <glad/glad.h>   // Criação de contexto OpenGL 3.3
+#include <GLFW/glfw3.h>  // Criação de janelas do sistema operacional
+
+// Headers da biblioteca GLM: criação de matrizes e vetores.
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Headers da biblioteca para carregar modelos obj
+#include <tiny_obj_loader.h>
+
+// Headers locais, definidos na pasta "include/"
+#include "utils\math_utils.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "graphics\stb_image.h"
+
+
+// Número de texturas carregadas pela função LoadTextureImage()
+GLuint g_NumLoadedTextures = 0;
+
+// Função que carrega uma imagem para ser utilizada como textura
+void LoadTextureImage(const char* filename)
+{
+    printf("Carregando imagem \"%s\"... ", filename);
+
+    // Primeiro fazemos a leitura da imagem do disco
+    stbi_set_flip_vertically_on_load(true);
+    int width;
+    int height;
+    int channels;
+    unsigned char *data = stbi_load(filename, &width, &height, &channels, 3);
+
+    if ( data == nullptr )
+    {
+        fprintf(stderr, "ERROR: Cannot open image file \"%s\".\n", filename);
+        std::exit(EXIT_FAILURE);
+    }
+
+    printf("OK (%dx%d).\n", width, height);
+
+    // Agora criamos objetos na GPU com OpenGL para armazenar a textura
+    GLuint texture_id;
+    GLuint sampler_id;
+    glGenTextures(1, &texture_id);
+    glGenSamplers(1, &sampler_id);
+
+    // Veja slides 95-96 do documento Aula_20_Mapeamento_de_Texturas.pdf
+    glSamplerParameteri(sampler_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glSamplerParameteri(sampler_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    // Parâmetros de amostragem da textura.
+    glSamplerParameteri(sampler_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glSamplerParameteri(sampler_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Agora enviamos a imagem lida do disco para a GPU
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+    glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+    glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
+
+    GLuint textureunit = g_NumLoadedTextures;
+    glActiveTexture(GL_TEXTURE0 + textureunit);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glBindSampler(textureunit, sampler_id);
+
+    stbi_image_free(data);
+
+    g_NumLoadedTextures += 1;
+}
